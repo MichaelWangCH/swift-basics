04695920a82f01c7b3d8287ab2d8372a3f8c0d92
Wang, Michael <ch_w@live.com>
2017-02-26 10:13:45 +0800

    Add uiplayground.playground

---
+++ uiplayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - Working with UIKit
+
+import UIKit
+
+let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+label.backgroundColor = UIColor.white
+label.text = "Hello Swift"
+label.textAlignment = .center
+label
+
